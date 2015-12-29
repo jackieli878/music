7570cd36ad9c72b55e8a221cf4e6d6088a0ae539
Jacqueline Li <jyl2164@columbia.edu>
2015-12-29 00:33:40 -0500

    initial commit

---
+++ php/searchAlbum.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p>Search</p>
+<form action="searchAlbumResults.php" method="post">
+   <input name="search" type="text" size="40" placeholder="Enter an album">
+   <input type = "submit" name="Submit" value="Search" >
+</form>
+</body>
+</html>
+
