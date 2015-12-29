a884ebaa6909affc7534f111855fb9250d1f2518
Jacqueline Li <jyl2164@columbia.edu>
2015-12-29 00:30:38 -0500

    initial commit

---
+++ php/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p>Search</p>
+<form action="searchResults.php" method="post">
+   <input name="search" type="text" size="40" placeholder="Search by artist, song name, album">
+   <input type = "submit" name="Submit" value="Search" >
+</form>
+</body>
+</html>
