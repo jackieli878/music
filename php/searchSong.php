b3e7e6a91cd42a792cfbcdbb3e4a877088b1ef2a
Jacqueline Li <jyl2164@columbia.edu>
2015-12-29 00:35:38 -0500

    initial commit

---
+++ php/searchSong.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p>Search</p>
+<form action="searchSongResults.php" method="post">
+   <input name="search" type="text" size="40" placeholder="Enter a song name">
+   <input type = "submit" name="Submit" value="Search" >
+</form>
+</body>
+</html>
+
